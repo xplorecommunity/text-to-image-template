5120880e9c73cbdc56c0761c1855833227083a0d
xplorecommunity <xplorecommunity1@gmail.com>
2025-09-14 23:45:57 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const queryPrompt = url.searchParams.get("text");
+    const prompt = queryPrompt && queryPrompt.trim() !== "" ? queryPrompt : "cat";
+
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt,
     };
 
     const response = await env.AI.run(
